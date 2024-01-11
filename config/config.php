8bcef78bac4b75e45116d32e1b5736197a673b5b
Abraham Sylvester <mrabblessedslyst4christ099@gmail.com>
2024-01-11 19:40:18 +0200

    deleted production-server connection variables

--- config/config.php
+++ config/config.php
@@ -1,9 +1,8 @@
 <?php
 	ob_start(); //Turn on output buffering
-	session_start(); // USED TO STOR THE SESSION VARIABLES WHEN INOUTS FAILS
+	session_start(); // USED TO STORE THE SESSION VARIABLES WHEN INPUTS FAILS
 	$timezone = date_default_timezone_set("Europe/London");
 	$con = mysqli_connect("localhost","root", "", "social");//Local Connection variable
-	// $con = mysqli_connect("localhost","sylabrah_boom_me", "@Admin", "social");// Server Connection variable
 	if(mysqli_connect_errno()){
 	echo "failed to connect: " . mysqli_connect_errno();
 }
